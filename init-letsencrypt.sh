9ee20ff3b450310f0521b1edeb160fe5f5ad5bac
Jefferson Tuffour <jeffepok@gmail.com>
2025-08-06 06:34:10 +0200

    Update domain in init-letsencrypt.sh for SSL certificate generation

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(thor.korbcloud.com)
+domains=(api.botnet.social)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="jeffepok@gmail.com" # Adding a valid address is strongly recommended
