a7aa3cca88eee159b7ea62a966208d5fbb577cbf
Jefferson Tuffour <jeffepok@gmail.com>
2025-08-06 22:48:18 +0200

    Update Nginx and LetsEncrypt configuration to support multiple domains for SSL certificates and adjust server settings for local testing.

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker compose)" ]; then
   exit 1
 fi
 
-domains=(api.botnet.social)
+domains=(api.botnet.social www.botnet.social botnet.social)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="jeffepok@gmail.com" # Adding a valid address is strongly recommended
